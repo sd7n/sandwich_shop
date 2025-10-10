a79331769d3e25830599420d3a8c3e001975122e
sd7n <SDedicatoria8@gmail.com>
2025-10-10 09:48:10 +0100

    Make center Const

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
+      body: const Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
